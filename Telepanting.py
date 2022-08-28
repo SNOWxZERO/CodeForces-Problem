21bc46a84b107ed715bdd8c08fa4c017bbd10292
SNOWxZERO <snowxzero@gmail.com>
2022-08-28 16:49:34 +0200

    Stealed from here :D https://blog.csdn.net/tianyizhicheng/article/details/119902977 COULDNT SOLVE BY MYSELF

--- Telepanting.py
+++ Telepanting.py
@@ -1,20 +1,35 @@
-from bisect import *
-n = int(input())
-x = [0] * n
-y = [0] * n
-s = [0] * n
-ps = [0] * (n + 1)
-for i in range(n):
-    x[i], y[i], s[i] = list(map(int, input().split()))
-ans = 0
+n = 0
+m = 200005
+r = [0] * m
+l = [0] * m
+s = [0] * m
+dp = [0] * m
+sum = [0] * m
+mod = 998244353
 
-for i in range(n):
-    dist = x[i] - y[i]
-    j = bisect(x, y[i])
-    cost = ps[i] - ps[j]
-    dp_i = cost + dist
-    ps[i + 1] = ps[i] + dp_i
-    ans += s[i] * dp_i
-ans += x[-1] + 1
-print(ans % 998244353)
-'TIME PROBLEM :{ I GIVE UP FOR NOW'
+
+def lower_bound(v):
+    l = 1
+    ri = n
+    ans = 0
+    while ri >= l:
+        mid = l + ri >> 1
+        if r[mid] >= v:
+            ri = mid - 1
+            ans = mid
+        else:
+            l = mid + 1
+    return ans
+
+
+n = int(input())
+for i in range(1, n + 1):
+    r[i], l[i], s[i] = map(int, input().split())
+ans = 0
+for i in range(1, n + 1):
+    ans = (ans + r[i] - r[i - 1]) % mod
+    p = lower_bound(l[i]) - 1
+    dp[i] = (r[i] - l[i] + sum[i - 1] - sum[p] + mod * 2) % mod
+    sum[i] = (sum[i - 1] + dp[i]) % mod
+    if s[i]: ans = (ans + dp[i]) % mod
+print((ans + 1) % mod)
